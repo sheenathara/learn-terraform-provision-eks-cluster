65cb8efa8bf468f6a69a887821dadaf9d9afe49a
Sheena T <sheena.tharakanparampil@harness.io>
2023-11-24 18:14:25 +0000

    9maint)

--- newone/subnets.tf
+++ newone/subnets.tf
@@ -1,22 +1,22 @@
-resource "aws_subnet" "private-us-east-1a" {
+resource "aws_subnet" "privateeks-us-east-1a" {
   vpc_id            = aws_vpc.maineks.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-1a"
 
   tags = {
-    "Name"                            = "private-us-east-1a"
+    "Name"                            = "privateeks-us-east-1a"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
 }
 
-resource "aws_subnet" "private-us-east-1b" {
+resource "aws_subnet" "privateeks-us-east-1b" {
   vpc_id            = aws_vpc.maineks.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-1b"
 
   tags = {
-    "Name"                            = "private-us-east-1b"
+    "Name"                            = "privateeks-us-east-1b"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
@@ -47,25 +47,25 @@ resource "aws_subnet" "publiceks-us-east-1b" {
     "kubernetes.io/cluster/demo" = "owned"
   }
 }
-resource "aws_subnet" "private-us-east-1a" {
+resource "aws_subnet" "privateeks-us-east-1a" {
   vpc_id            = aws_vpc.maineks.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-1a"
 
   tags = {
-    "Name"                            = "private-us-east-1a"
+    "Name"                            = "privateeks-us-east-1a"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
 }
 
-resource "aws_subnet" "private-us-east-1b" {
+resource "aws_subnet" "privateeks-us-east-1b" {
   vpc_id            = aws_vpc.maineks.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-1b"
 
   tags = {
-    "Name"                            = "private-us-east-1b"
+    "Name"                            = "privateeks-us-east-1b"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/demo"      = "owned"
   }
