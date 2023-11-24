9ef574ea0094543b4787989891dbc3299b3b08ea
Sheena T <sheena.tharakanparampil@harness.io>
2023-11-24 18:22:38 +0000

    (maint)

--- newone/subnets.tf
+++ newone/subnets.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "privateeks-us-east-1a" {
-  vpc_id            = aws_vpc.maineks.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/19"
   availability_zone = "us-east-1a"
 
@@ -11,7 +11,7 @@ resource "aws_subnet" "privateeks-us-east-1a" {
 }
 
 resource "aws_subnet" "privateeks-us-east-1b" {
-  vpc_id            = aws_vpc.maineks.id
+  vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.32.0/19"
   availability_zone = "us-east-1b"
 
@@ -23,10 +23,10 @@ resource "aws_subnet" "privateeks-us-east-1b" {
 }
 
 resource "aws_subnet" "publiceks-us-east-1a" {
-  vpc_id                  = aws_vpc.maineks.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
   availability_zone       = "us-east-1a"
-  map_publiceks_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
     "Name"                       = "publiceks-us-east-1a"
@@ -36,59 +36,10 @@ resource "aws_subnet" "publiceks-us-east-1a" {
 }
 
 resource "aws_subnet" "publiceks-us-east-1b" {
-  vpc_id                  = aws_vpc.maineks.id
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.96.0/19"
   availability_zone       = "us-east-1b"
-  map_publiceks_ip_on_launch = true
-
-  tags = {
-    "Name"                       = "publiceks-us-east-1b"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
-  }
-}
-resource "aws_subnet" "privateeks-us-east-1a" {
-  vpc_id            = aws_vpc.maineks.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    "Name"                            = "privateeks-us-east-1a"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
-}
-
-resource "aws_subnet" "privateeks-us-east-1b" {
-  vpc_id            = aws_vpc.maineks.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    "Name"                            = "privateeks-us-east-1b"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/demo"      = "owned"
-  }
-}
-
-resource "aws_subnet" "publiceks-us-east-1a" {
-  vpc_id                  = aws_vpc.maineks.id
-  cidr_block              = "10.0.64.0/19"
-  availability_zone       = "us-east-1a"
-  map_publiceks_ip_on_launch = true
-
-  tags = {
-    "Name"                       = "publiceks-us-east-1a"
-    "kubernetes.io/role/elb"     = "1"
-    "kubernetes.io/cluster/demo" = "owned"
-  }
-}
-
-resource "aws_subnet" "publiceks-us-east-1b" {
-  vpc_id                  = aws_vpc.maineks.id
-  cidr_block              = "10.0.96.0/19"
-  availability_zone       = "us-east-1b"
-  map_publiceks_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
     "Name"                       = "publiceks-us-east-1b"
